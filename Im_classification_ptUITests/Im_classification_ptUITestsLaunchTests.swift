cce99a847cab177cac4ad834fa930e4422506a12
El Mehdi Selbi <36630821+mehdiselbi@users.noreply.github.com>
2024-04-02 00:23:41 -0400

    init

---
+++ Im_classification_ptUITests/Im_classification_ptUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Im_classification_ptUITestsLaunchTests.swift
+//  Im_classification_ptUITests
+//
+//  Created by Mehdi Selbi on 1/4/2024.
+//
+
+import XCTest
+
+final class Im_classification_ptUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Im_classification_pt' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'LibTorch'
+
+  # Pods for Im_classification_pt
+
+  target 'Im_classification_ptTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Im_classification_ptUITests' do
+    # Pods for testing
+  end
+
+end
